10a5cf2d0d9c86beacaa51c0bb7ce7d569af24b9
Ordaz Romero Sergio <orcheko@gmail.com>
2024-06-08 17:54:35 -0600

    Fix email to unique

--- applications/users/models.py
+++ applications/users/models.py
@@ -1,10 +1,12 @@
 from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class User(AbstractUser):
     slug = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name='slug', db_index=True)
+    email = models.EmailField(_("email address"), unique=True, null=False, blank=False)
 
     class Meta:
         db_table = "auth_user"
