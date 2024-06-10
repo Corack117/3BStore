5dc22ceec71e470d656307277e6f36a474bb7c6c
Ordaz Romero Sergio <orcheko@gmail.com>
2024-06-09 22:15:36 -0600

    Fix logger

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 import logging
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
